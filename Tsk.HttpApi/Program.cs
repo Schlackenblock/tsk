0b9e93eb4b3f65f59c02053f10817bbfbe6ac1d5
Vlad <60189579+Lempapo@users.noreply.github.com>
2024-02-27 16:06:12 +0300

    Synchronise summary and temperature. (#2)

--- Tsk.HttpApi/Program.cs
+++ Tsk.HttpApi/Program.cs
@@ -18,23 +18,43 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+string SelectSummary(int temperatureC)
+{
+    var summary = temperatureC switch
+    {
+        <= -12 => summaries[0],
+        <= -5 => summaries[1],
+        <= 2 => summaries[2],
+        <= 10 => summaries[3],
+        <= 17 => summaries[4],
+        <= 25 => summaries[5],
+        <= 32 => summaries[6],
+        <= 40 => summaries[7],
+        <= 47 => summaries[8],
+        _ => summaries[9]
+    };
+    return summary;
+}
+
 app.MapGet("/weatherforecast", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
+        {
+            var temperatureRange = Random.Shared.Next(-20, 55);
+            return new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                temperatureRange,
+                SelectSummary(temperatureRange)
+            );
+        }).ToArray();
     return forecast;
 });
 
 app.Run();
 
 [PublicAPI]
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
