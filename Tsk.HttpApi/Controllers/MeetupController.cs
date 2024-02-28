cade8443826c42c81151a1681dcf1c549aa7f617
lempapo <vlad.lemtyugov02@gmail.com>
2024-02-28 19:55:05 +0300

    Fix code formatting.

--- Tsk.HttpApi/Controllers/MeetupController.cs
+++ Tsk.HttpApi/Controllers/MeetupController.cs
@@ -5,54 +5,53 @@ namespace Tsk.HttpApi.Controllers;
 [ApiController]
 [Route("/meetups")]
 public class MeetupController : ControllerBase
-{
-     private static readonly ICollection<Meetup> Meetups  = new List<Meetup>();
+{ 
+    private static readonly ICollection<Meetup> Meetups = new List<Meetup>();
 
-     [HttpGet]
-     public IActionResult GetMeetup()
-     {
+    [HttpGet]
+    public IActionResult GetMeetup() 
+    { 
           return Ok(Meetups);
-     }
+    }
      
-     [HttpPost]
-     public IActionResult PostMeetup([FromBody] Meetup newMeetup)
-     {
-          newMeetup.Id = Guid.NewGuid();
-          Meetups.Add(newMeetup);
+    [HttpPost]
+    public IActionResult PostMeetup([FromBody] Meetup newMeetup)
+    {
+         newMeetup.Id = Guid.NewGuid();
+         Meetups.Add(newMeetup);
+         
+         return Ok(newMeetup);
+    }
 
-          return Ok(newMeetup);
-     }
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteMeetup([FromRoute] Guid id)
+    {
+         var meetupToDelete = Meetups.SingleOrDefault(meetup => meetup.Id == id);
 
-     [HttpDelete("{id:guid}")]
-     public IActionResult DeleteMeetup([FromRoute] Guid id)
-     {
-          var meetupToDelete = Meetups.SingleOrDefault(meetup => meetup.Id == id);
+         if (meetupToDelete is null)
+         {
+              return NotFound();
+         }
 
-          if (meetupToDelete is null)
-          {
-               return NotFound();
-          }
-
-          Meetups.Remove(meetupToDelete);
-          return Ok(meetupToDelete);
-     }
+         Meetups.Remove(meetupToDelete);
+         return Ok(meetupToDelete);
+    }
      
-     [HttpPut("{id:guid}")]
-     public IActionResult UpdateMeetup([FromRoute] Guid id, [FromBody] Meetup updatedMeetup)
-     {
-          var oldMeetup = Meetups.SingleOrDefault(meetup => meetup.Id == id);
-
+    [HttpPut("{id:guid}")]
+    public IActionResult UpdateMeetup([FromRoute] Guid id, [FromBody] Meetup updatedMeetup)
+    {
+         var oldMeetup = Meetups.SingleOrDefault(meetup => meetup.Id == id);
           // meetup with provided id does not exist
-          if (oldMeetup is null)
-          {
-               return NotFound();
-          }
+         if (oldMeetup is null)
+         {
+              return NotFound();
+         }
 
-          oldMeetup.Topic = updatedMeetup.Topic;
-          oldMeetup.Place = updatedMeetup.Place;
-          oldMeetup.Duration = updatedMeetup.Duration;
+         oldMeetup.Topic = updatedMeetup.Topic;
+         oldMeetup.Place = updatedMeetup.Place;
+         oldMeetup.Duration = updatedMeetup.Duration;
 
-          return NoContent();
+         return NoContent();
      }
      
      public class Meetup
