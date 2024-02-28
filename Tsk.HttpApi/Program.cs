0db9a3976b7795234a18e3f6ac847f4682a30c8e
Vlad <60189579+Lempapo@users.noreply.github.com>
2024-02-28 14:51:52 +0300

    Remove weatherforecast. (#4)

--- Tsk.HttpApi/Program.cs
+++ Tsk.HttpApi/Program.cs
@@ -1,5 +1,3 @@
-using JetBrains.Annotations;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -13,38 +11,5 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        {
-            var temperatureC = Random.Shared.Next(-20, 55);
-            return new WeatherForecast
-            (
-                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                temperatureC
-            );
-        }).ToArray();
-    return forecast;
-});
-
 app.Run();
 
-[PublicAPI]
-internal record WeatherForecast(DateOnly Date, int TemperatureC)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-    public string Summary => TemperatureC switch
-    {
-        <= -10 => "Freezing",
-        <= 0 => "Cold",
-        <= 4 => "Chilly",
-        <= 8 => "Brisk",
-        <= 13 => "Cool",
-        <= 18 => "Mild",
-        <= 25 => "Perfect",
-        <= 29 => "Warm",
-        <= 33 => "Hot",
-        _ => "Roasting"
-    };
-}
