0de10e11123bc9c3e3958857023f99dd8fb740ce
lempapo <vlad.lemtyugov02@gmail.com>
2024-02-29 17:22:59 +0300

    Translation of HTTP requests to DTO.

--- Tsk.HttpApi/Meetups/Controller.cs
+++ Tsk.HttpApi/Meetups/Controller.cs
@@ -1,4 +1,3 @@
-using JetBrains.Annotations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Tsk.HttpApi.Meetups;
@@ -14,48 +13,69 @@ public class MeetupController : ControllerBase
         Ok(meetups);
 
     [HttpPost]
-    public IActionResult PostMeetup([FromBody] Meetup newMeetup)
+    public IActionResult CreateMeetup([FromBody] CreateMeetupDto createDto)
     {
-        newMeetup.Id = Guid.NewGuid();
+        var newMeetup = new Meetup
+        {
+            Id = Guid.NewGuid(),
+            Topic = createDto.Topic,
+            Place = createDto.Place,
+            Duration = createDto.Duration
+        };
         meetups.Add(newMeetup);
-        return Ok(newMeetup);
+
+        var readDto = new ReadMeetupDto
+        {
+            Id = newMeetup.Id,
+            Topic = newMeetup.Topic,
+            Place = newMeetup.Place,
+            Duration = newMeetup.Duration
+        };
+        return Ok(readDto);
     }
 
     [HttpDelete("{id:guid}")]
     public IActionResult DeleteMeetup([FromRoute] Guid id)
     {
         var meetupToDelete = meetups.SingleOrDefault(meetup => meetup.Id == id);
+
         if (meetupToDelete is null)
         {
             return NotFound();
         }
-
         meetups.Remove(meetupToDelete);
-        return Ok(meetupToDelete);
+
+        var readDto = new ReadMeetupDto
+        {
+            Id = meetupToDelete.Id,
+            Topic = meetupToDelete.Topic,
+            Place = meetupToDelete.Place,
+            Duration = meetupToDelete.Duration
+        };
+
+        return Ok(readDto);
     }
 
     [HttpPut("{id:guid}")]
-    public IActionResult UpdateMeetup([FromRoute] Guid id, [FromBody] Meetup updatedMeetup)
+    public IActionResult UpdateMeetup([FromRoute] Guid id, [FromBody] UpdateMeetupDto updateMeetupDto)
     {
         var oldMeetup = meetups.SingleOrDefault(meetup => meetup.Id == id);
         if (oldMeetup is null)
         {
             return NotFound();
         }
+        oldMeetup.Topic = updateMeetupDto.Topic;
+        oldMeetup.Place = updateMeetupDto.Place;
+        oldMeetup.Duration = updateMeetupDto.Duration;
 
-        oldMeetup.Topic = updatedMeetup.Topic;
-        oldMeetup.Place = updatedMeetup.Place;
-        oldMeetup.Duration = updatedMeetup.Duration;
+        var readDto = new ReadMeetupDto
+        {
+            Id = oldMeetup.Id,
+            Topic = updateMeetupDto.Topic,
+            Place = updateMeetupDto.Place,
+            Duration = updateMeetupDto.Duration
+        };
 
-        return NoContent();
-    }
-
-    [PublicAPI]
-    public class Meetup
-    {
-        public Guid? Id { get; set; }
-        public required string Topic { get; set; }
-        public required string Place { get; set; }
-        public required int Duration { get; set; }
+        return Ok(readDto);
     }
 }
