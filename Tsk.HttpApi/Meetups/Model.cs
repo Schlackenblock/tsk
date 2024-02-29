35bb8590ac84c9c555547424d6179a8090b39805
lempapo <vlad.lemtyugov02@gmail.com>
2024-02-29 15:51:45 +0300

    Change access modifiers and data type.

--- Tsk.HttpApi/Meetups/Model.cs
+++ Tsk.HttpApi/Meetups/Model.cs
@@ -2,7 +2,7 @@ namespace Tsk.HttpApi.Meetups;
 
 internal class Meetup
 {
-    public Guid? Id { get; set; }
+    public Guid Id { get; set; }
     public required string Topic { get; set; }
     public required string Place { get; set; }
     public required int Duration { get; set; }
