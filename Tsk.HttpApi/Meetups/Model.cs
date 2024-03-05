3d49c2174bcf73e7f47300c0f3abd5ee4dd3911f
lempapo <vlad.lemtyugov02@gmail.com>
2024-03-05 18:58:15 +0300

    https://youtube.com/shorts/eHatE89AQ5U?si=bj67ygU3-jhkCJpH

--- Tsk.HttpApi/Meetups/Model.cs
+++
@@ -1,9 +0,0 @@
-namespace Tsk.HttpApi.Meetups;
-
-internal class Meetup
-{
-    public Guid Id { get; init; }
-    public required string Topic { get; set; }
-    public required string Place { get; set; }
-    public required int Duration { get; set; }
-}
