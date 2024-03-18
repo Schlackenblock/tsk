134096d744b67c1b84976a16215f82660450c412
lempapo <vlad.lemtyugov02@gmail.com>
2024-03-18 14:39:33 +0300

    Fix code mistakes.

--- Tsk.HttpApi/DatabaseContext.cs
+++ Tsk.HttpApi/DatabaseContext.cs
@@ -5,7 +5,7 @@ namespace Tsk.HttpApi;
 
 internal class DatabaseContext : DbContext
 {
-    public DbSet<ProductEntity>? Products { get; }
+    public DbSet<ProductEntity> Products => Set<ProductEntity>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder options) =>
         options.UseNpgsql("Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=postgres");
--- Tsk.HttpApi/Products/DataTransferObjects.cs
+++ Tsk.HttpApi/Products/DataTransferObjects.cs
@@ -5,19 +5,19 @@ namespace Tsk.HttpApi.Products;
 [PublicAPI]
 public record ReadProductDto(Guid Id, string Title, string Description, double Price)
 {
-    /// <summary>ProductEntity id.</summary>
+    /// <summary>Product id.</summary>
     /// <example>xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx</example>
     public Guid Id { get; init; } = Id;
 
-    /// <summary>ProductEntity title.</summary>
+    /// <summary>Product title.</summary>
     /// <example>Schlackenblock.</example>
     public string Title { get; init; } = Title;
 
-    /// <summary>ProductEntity description.</summary>
+    /// <summary>Product description.</summary>
     /// <example>Solid concrete brick M200 graphite 250x120x65.</example>
     public string Description { get; init; } = Description;
 
-    /// <summary>ProductEntity price.</summary>
+    /// <summary>Product price.</summary>
     /// <example>1.8</example>
     public double Price { get; init; } = Price;
 }
@@ -25,15 +25,15 @@ public record ReadProductDto(Guid Id, string Title, string Description, double P
 [PublicAPI]
 public record CreateProductDto(string Title, string Description, double Price)
 {
-    /// <summary>ProductEntity title.</summary>
+    /// <summary>Product title.</summary>
     /// <example>Schlackenblock.</example>
     public string Title { get; init; } = Title;
 
-    /// <summary>ProductEntity description.</summary>
+    /// <summary>Product description.</summary>
     /// <example>Solid concrete brick M200 graphite 250x120x65.</example>
     public string Description { get; init; } = Description;
 
-    /// <summary>ProductEntity price.</summary>
+    /// <summary>Product price.</summary>
     /// <example>1.8</example>
     public double Price { get; init; } = Price;
 }
@@ -41,15 +41,15 @@ public record CreateProductDto(string Title, string Description, double Price)
 [PublicAPI]
 public record UpdateProductDto(string Title, string Description, double Price)
 {
-    /// <summary>ProductEntity title.</summary>
+    /// <summary>Product title.</summary>
     /// <example>Schlackenblock.</example>
     public string Title { get; init; } = Title;
 
-    /// <summary>ProductEntity description.</summary>
+    /// <summary>Product description.</summary>
     /// <example>Solid concrete brick M200 graphite 250x120x65.</example>
     public string Description { get; init; } = Description;
 
-    /// <summary>ProductEntity price.</summary>
+    /// <summary>Product price.</summary>
     /// <example>1.8</example>
     public double Price { get; init; } = Price;
 }
