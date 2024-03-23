eaea15f7ddb4ae99184b4962687f928dfaa1899b
Bogdan Shahnitsky <satma0745@gmail.com>
2024-03-23 23:42:04 +0300

    Revert "Purposefully trigger action failure."
    This reverts commit feea558d5a42fc718469760831d9d08cd52fe8c2.

--- Tsk.HttpApi/Products/Model.cs
+++ Tsk.HttpApi/Products/Model.cs
@@ -6,7 +6,4 @@ internal class ProductEntity
     public required string Title { get; set; }
     public required string Description { get; set; }
     public required double Price { get; set; }
-
-    // TODO: Remove immediately.
-    public int TestProperty { get; set; }
 }
