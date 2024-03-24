c6a66dbf55584830c06abb61c2ef8a73f7cadc48
lempapo <vlad.lemtyugov02@gmail.com>
2024-03-24 16:58:29 +0300

    Add validation.

--- Tsk.HttpApi/Products/DataTransferObjects.cs
+++ Tsk.HttpApi/Products/DataTransferObjects.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using JetBrains.Annotations;
 
 namespace Tsk.HttpApi.Products;
@@ -14,8 +15,17 @@ public class ProductDto
 [PublicAPI]
 public class CreateProductDto
 {
+    [Required]
+    [MaxLength(100)]
+    [RegularExpression(@"^[\w\s\.-–—]*$")]
     public required string Title { get; init; }
+
+    [Required]
+    [MaxLength(100)]
+    [RegularExpression(@"^[\w\s\.\d]*")]
     public required string Description { get; init; }
+
+    [Required]
     public required double Price { get; init; }
 }
 
