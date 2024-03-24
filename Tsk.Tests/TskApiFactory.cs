d61061c4ea9a0cffe0ba8e8368ad8f18359b82c7
Bogdan Shahnitsky <satma0745@gmail.com>
2024-03-24 23:37:44 +0300

    Improve the `TskApiFactory`.

--- Tsk.Tests/TskApiFactory.cs
+++ Tsk.Tests/TskApiFactory.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Testcontainers.PostgreSql;
 using Tsk.HttpApi;
 using Xunit;
@@ -8,25 +12,40 @@ namespace Tsk.Tests;
 
 public class TskApiFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
-    public TskContext Context => new TskContext(dbContextOptionsSubstitution);
-
-    private readonly PostgreSqlContainer postgreSqlContainer;
-    private readonly DbContextOptions<TskContext> dbContextOptionsSubstitution;
-
-    public TskApiFactory()
-    {
-        postgreSqlContainer = new PostgreSqlBuilder()
+    private readonly PostgreSqlContainer postgreSqlContainer =
+        new PostgreSqlBuilder()
             .WithImage("postgres:latest")
             .Build();
 
-        dbContextOptionsSubstitution = new DbContextOptionsBuilder<TskContext>()
-            .UseNpgsql(postgreSqlContainer.GetConnectionString())
-            .Options;
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(
+            services =>
+            {
+                services.RemoveAll<DbContextOptions<TskContext>>();
+
+                var dbContextOptionsSubstitution =
+                    new DbContextOptionsBuilder<TskContext>()
+                        .UseNpgsql(postgreSqlContainer.GetConnectionString())
+                        .Options;
+
+                services.AddSingleton(dbContextOptionsSubstitution);
+            }
+        );
+    }
+
+    public TskContext CreateDbContext()
+    {
+        var dbContextOptionsSubstitution = Services.GetRequiredService<DbContextOptions<TskContext>>();
+        return new TskContext(dbContextOptionsSubstitution);
     }
 
     public async Task InitializeAsync()
     {
         await postgreSqlContainer.StartAsync();
+
+        var context = CreateDbContext();
+        await context.Database.EnsureCreatedAsync();
     }
 
     public new async Task DisposeAsync()
