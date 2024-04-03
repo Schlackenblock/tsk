f195a25c13ba09c2364f37a5ef7df34eacb4121b
Bogdan Shahnitsky <satma0745@gmail.com>
2024-04-03 15:35:58 +0300

    Introduce integration tests. (#23)

---
+++ Tsk.Tests/Products/DeleteProductTestSuite.cs
@@ -0,0 +1,38 @@
+using Tsk.HttpApi.Products;
+
+namespace Tsk.Tests.Products;
+
+public class DeleteProductTestSuite : TestSuiteBase
+{
+    [Fact]
+    public async Task DeleteProduct_WhenProductExists_ShouldSucceed()
+    {
+        var productId = Guid.NewGuid();
+        var existingProduct = new ProductEntity
+        {
+            Id = productId,
+            Title = "High Performance Concrete Admixture 20 lbs",
+            Description = "20 LB. BAG - High Performance Admixture for Concrete - gray color",
+            Price = 47
+        };
+        Context.Products.Add(existingProduct);
+        await Context.SaveChangesAsync();
+
+        var response = await HttpClient.DeleteAsync($"/products/{productId}");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var deletedProductDto = await response.Content.ReadFromJsonAsync<ProductDto>();
+        deletedProductDto.Should().BeEquivalentTo(existingProduct);
+
+        var persistedProducts = await Context.Products.ToListAsync();
+        persistedProducts.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task DeleteProduct_WhenProductDoesNotExist_ShouldFail()
+    {
+        var notExistingProductId = Guid.NewGuid();
+        var response = await HttpClient.DeleteAsync($"/products/{notExistingProductId}");
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
---
+++ Tsk.Tests/Products/GetProductsTestSuite.cs
@@ -0,0 +1,66 @@
+using Tsk.HttpApi.Products;
+
+namespace Tsk.Tests.Products;
+
+public class GetProductsTestSuite : TestSuiteBase
+{
+    [Fact]
+    public async Task GetProducts_WhenManyExist_ShouldReturnMany()
+    {
+        var existingProducts = new List<ProductEntity>
+        {
+            new ProductEntity
+            {
+                Id = Guid.NewGuid(),
+                Title = "High Performance Concrete Admixture 20 lbs",
+                Description = "20 LB. BAG - High Performance Admixture for Concrete - gray color",
+                Price = 47
+            },
+            new ProductEntity
+            {
+                Id = Guid.NewGuid(),
+                Title = "High Performance Concrete Admixture 10 lbs",
+                Description = "10 LB. BAG - High Performance Admixture for Concrete - gray color",
+                Price = 28
+            }
+        };
+        Context.Products.AddRange(existingProducts);
+        await Context.SaveChangesAsync();
+
+        var response = await HttpClient.GetAsync("/products");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productDtos = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+        productDtos.Should().BeEquivalentTo(existingProducts);
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenOneExists_ShouldReturnOne()
+    {
+        var existingProduct = new ProductEntity
+        {
+            Id = Guid.NewGuid(),
+            Title = "High Performance Concrete Admixture 20 lbs",
+            Description = "20 LB. BAG - High Performance Admixture for Concrete - gray color",
+            Price = 47
+        };
+        Context.Products.Add(existingProduct);
+        await Context.SaveChangesAsync();
+
+        var response = await HttpClient.GetAsync("/products");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productDtos = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+        productDtos!.Single().Should().BeEquivalentTo(existingProduct);
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenNoneExist_ShouldReturnNone()
+    {
+        var response = await HttpClient.GetAsync("/products");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productDtos = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+        productDtos.Should().BeEmpty();
+    }
+}
