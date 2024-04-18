22e5754df121de758da9101d13e09b13cfbf4115
Bogdan Shahnitsky <satma0745@gmail.com>
2024-04-18 20:57:03 +0300

    Fix `GetProducts` endpoint tests.

--- Tsk.Tests/Products/GetProductsTestSuite.cs
+++ Tsk.Tests/Products/GetProductsTestSuite.cs
@@ -5,40 +5,159 @@ namespace Tsk.Tests.Products;
 public class GetProductsTestSuite : TestSuiteBase
 {
     [Fact]
-    public async Task GetProducts_WhenManyExist_ShouldReturnMany()
+    public async Task GetProducts_WhenOrderingApplied_ShouldReturnOrdered()
     {
-        var existingProducts = ProductTestData.GenerateProducts(2);
-        Context.Products.AddRange(existingProducts);
-        await Context.SaveChangesAsync();
+        var supportedOrderingOptions = new (string, Func<IEnumerable<ProductEntity>, IEnumerable<ProductEntity>>)[]
+        {
+            ("TitleAscending", products => products.OrderBy(product => product.Title)),
+            ("TitleDescending", products => products.OrderByDescending(product => product.Title)),
+            ("PriceAscending", products => products.OrderBy(product => product.Price)),
+            ("PriceDescending", products => products.OrderByDescending(product => product.Price))
+        };
 
-        var response = await HttpClient.GetAsync("/products");
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        foreach (var supportedOrderingOption in supportedOrderingOptions)
+        {
+            var (orderingOptionName, sortProducts) = supportedOrderingOption;
 
-        var productDtos = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
-        productDtos.Should().BeEquivalentTo(existingProducts);
+            var existingProducts = ProductTestData.GenerateProducts(10);
+            Context.Products.AddRange(existingProducts);
+            await Context.SaveChangesAsync();
+
+            var requestUri = $"/products?OrderingOption={orderingOptionName}&PageNumber=0&PageSize=10";
+            var response = await HttpClient.GetAsync(requestUri);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+            productsPageDto!.ProductsCount.Should().Be(existingProducts.Count);
+            productsPageDto.PagesCount.Should().Be(1);
+
+            var orderedExistingProducts = sortProducts(existingProducts);
+            var productDtos = productsPageDto.Products;
+            productDtos.Should().BeEquivalentTo(orderedExistingProducts, options => options.WithStrictOrdering());
+
+            await Context.Products.ExecuteDeleteAsync();
+        }
     }
 
     [Fact]
-    public async Task GetProducts_WhenOneExists_ShouldReturnOne()
+    public async Task GetProducts_WhenFiltersApplied_ShouldReturnFiltered()
+    {
+        var minPrice = 10.00;
+        var maxPrice = 100.00;
+        var existingProducts = new
+        {
+            ThatWillBeFilteredOutByMinPrice = ProductTestData.GenerateProducts(
+                count: 5,
+                priceRange: new() { Max = 9.99 }
+            ),
+            ThatWillBeFilteredOutByMaxPrice = ProductTestData.GenerateProducts(
+                count: 5,
+                priceRange: new() { Min = 99.99 }
+            ),
+            ThatWillNotBeFilteredOut = ProductTestData.GenerateProducts(
+                count: 15,
+                priceRange: new() { Min = minPrice, Max = maxPrice }
+            )
+        };
+        Context.Products.AddRange(existingProducts.ThatWillBeFilteredOutByMinPrice);
+        Context.Products.AddRange(existingProducts.ThatWillBeFilteredOutByMaxPrice);
+        Context.Products.AddRange(existingProducts.ThatWillNotBeFilteredOut);
+        await Context.SaveChangesAsync();
+
+        var pageNumber = 1;
+        var pageSize = 10;
+
+        var requestUri =
+            "/products?" +
+            "OrderingOption=TitleAscending" +
+            $"&PageNumber={pageNumber}" +
+            $"&PageSize={pageSize}" +
+            $"&MinPrice={minPrice}" +
+            $"&MaxPrice={maxPrice}";
+        var response = await HttpClient.GetAsync(requestUri);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+
+        var expectedProductsCount = existingProducts.ThatWillNotBeFilteredOut.Count;
+        productsPageDto!.ProductsCount.Should().Be(expectedProductsCount);
+
+        var expectedPagesCount = (int)(Math.Ceiling((double)expectedProductsCount / pageSize));
+        productsPageDto.PagesCount.Should().Be(expectedPagesCount);
+
+        var expectedProducts = existingProducts
+            .ThatWillNotBeFilteredOut
+            .OrderBy(product => product.Title)
+            .Skip(pageNumber * pageSize)
+            .Take(pageSize)
+            .ToList();
+        var returnedProductDtos = productsPageDto.Products;
+        returnedProductDtos.Should().BeEquivalentTo(expectedProducts);
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenPageCountExceeded_ShouldReturnNone()
     {
         var existingProduct = ProductTestData.GenerateProduct();
         Context.Products.Add(existingProduct);
         await Context.SaveChangesAsync();
 
-        var response = await HttpClient.GetAsync("/products");
+        var response = await HttpClient.GetAsync("/products?OrderingOption=TitleAscending&PageNumber=1&PageSize=10");
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        var productDtos = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
-        productDtos!.Single().Should().BeEquivalentTo(existingProduct);
+        var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+        productsPageDto!.PagesCount.Should().Be(1);
+        productsPageDto.ProductsCount.Should().Be(1);
+        productsPageDto.Products.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenFilteredOutByMinPrice_ShouldReturnNone()
+    {
+        var existingProduct = ProductTestData.GenerateProduct();
+        Context.Products.Add(existingProduct);
+        await Context.SaveChangesAsync();
+
+        var minPrice = existingProduct.Price + 0.01;
+        var response = await HttpClient.GetAsync(
+            $"/products?OrderingOption=TitleAscending&PageNumber=0&PageSize=10&MinPrice={minPrice}"
+        );
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+        productsPageDto!.PagesCount.Should().Be(0);
+        productsPageDto.ProductsCount.Should().Be(0);
+        productsPageDto.Products.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenFilteredOutByMaxPrice_ShouldReturnNone()
+    {
+        var existingProduct = ProductTestData.GenerateProduct();
+        Context.Products.Add(existingProduct);
+        await Context.SaveChangesAsync();
+
+        var maxPrice = existingProduct.Price - 0.01;
+        var response = await HttpClient.GetAsync(
+            $"/products?OrderingOption=TitleAscending&PageNumber=0&PageSize=10&MaxPrice={maxPrice}"
+        );
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+        productsPageDto!.PagesCount.Should().Be(0);
+        productsPageDto.ProductsCount.Should().Be(0);
+        productsPageDto.Products.Should().BeEmpty();
     }
 
     [Fact]
     public async Task GetProducts_WhenNoneExist_ShouldReturnNone()
     {
-        var response = await HttpClient.GetAsync("/products");
+        var response = await HttpClient.GetAsync("/products?OrderingOption=TitleAscending&PageNumber=0&PageSize=10");
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        var productDtos = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
-        productDtos.Should().BeEmpty();
+        var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+        productsPageDto!.PagesCount.Should().Be(0);
+        productsPageDto.ProductsCount.Should().Be(0);
+        productsPageDto.Products.Should().BeEmpty();
     }
 }
--- Tsk.Tests/Products/ProductTestData.cs
+++ Tsk.Tests/Products/ProductTestData.cs
@@ -8,31 +8,37 @@ public static class ProductTestData
     private static readonly Faker faker = new();
 
     public static ProductEntity GenerateProduct() =>
+        GenerateProduct(PriceRange.Default);
+
+    private static ProductEntity GenerateProduct(PriceRange priceRange) =>
         new ProductEntity
         {
             Id = GenerateProductId(),
             Title = GenerateProductTitle(),
-            Price = GenerateProductPrice()
+            Price = GenerateProductPrice(priceRange)
         };
 
     public static IReadOnlyCollection<ProductEntity> GenerateProducts(int count) =>
+        GenerateProducts(count, PriceRange.Default);
+
+    public static IReadOnlyCollection<ProductEntity> GenerateProducts(int count, PriceRange priceRange) =>
         Enumerable
             .Range(0, count)
-            .Select(_ => GenerateProduct())
+            .Select(_ => GenerateProduct(priceRange))
             .ToList();
 
     public static CreateProductDto GenerateCreateProductDto() =>
         new CreateProductDto
         {
             Title = GenerateProductTitle(),
-            Price = GenerateProductPrice()
+            Price = GenerateProductPrice(PriceRange.Default)
         };
 
     public static UpdateProductDto GenerateUpdateProductDto() =>
         new UpdateProductDto
         {
             Title = GenerateProductTitle(),
-            Price = GenerateProductPrice()
+            Price = GenerateProductPrice(PriceRange.Default)
         };
 
     private static Guid GenerateProductId() =>
@@ -41,11 +47,20 @@ public static class ProductTestData
     private static string GenerateProductTitle() =>
         faker.Commerce.ProductName();
 
-    private static double GenerateProductPrice(double floor = 0.01, double ceiling = 10_000.00)
+    private static double GenerateProductPrice(PriceRange priceRange)
     {
-        var floorInCents = (int)(floor * 100);
-        var ceilingInCents = (int)(ceiling * 100);
+        var floorInCents = (int)(priceRange.Min * 100);
+        var ceilingInCents = (int)(priceRange.Max * 100);
         var priceInCents = Random.Shared.Next(floorInCents, ceilingInCents);
         return priceInCents / 100d;
     }
+}
+
+public class PriceRange
+{
+    public static PriceRange Default =>
+        new();
+
+    public double Min { get; init; } = 0.01;
+    public double Max { get; init; } = 10_000.00;
 }
