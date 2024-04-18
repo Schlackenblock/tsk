532ae3ee93e592986ad315f5ecd807f11e7f6e60
Bogdan Shahnitsky <satma0745@gmail.com>
2024-04-18 18:54:21 +0300

    Test the `GetProduct` endpoint.

---
+++ Tsk.Tests/Products/GetProductTestSuite.cs
@@ -0,0 +1,28 @@
+﻿using Tsk.HttpApi.Products;
+
+namespace Tsk.Tests.Products;
+
+public class GetProductTestSuite : TestSuiteBase
+{
+    [Fact]
+    public async Task GetProduct_WhenProductExists_ShouldSucceed()
+    {
+        var existingProduct = ProductTestData.GenerateProduct();
+        Context.Products.Add(existingProduct);
+        await Context.SaveChangesAsync();
+
+        var response = await HttpClient.GetAsync($"/products/{existingProduct.Id}");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productDto = await response.Content.ReadFromJsonAsync<ProductDto>();
+        productDto.Should().BeEquivalentTo(existingProduct);
+    }
+
+    [Fact]
+    public async Task GetProduct_WhenProductDoesNotExist_ShouldFail()
+    {
+        var notExistingProductId = Guid.NewGuid();
+        var response = await HttpClient.GetAsync($"/products/{notExistingProductId}");
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
