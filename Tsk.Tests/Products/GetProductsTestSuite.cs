5bf9aa7a896a6032fa0a962f595a119d80785c61
Bogdan Shahnitsky <satma0745@gmail.com>
2024-04-20 14:48:01 +0300

    Improve tests for the `GetProducts` endpoint.

--- Tsk.Tests/Products/GetProductsTestSuite.cs
+++ Tsk.Tests/Products/GetProductsTestSuite.cs
@@ -5,40 +5,99 @@ namespace Tsk.Tests.Products;
 public class GetProductsTestSuite : TestSuiteBase
 {
     [Fact]
-    public async Task GetProducts_WhenOrderingApplied_ShouldReturnOrdered()
+    public async Task GetProducts_WhenTitleAscendingOrderingApplied_ShouldReturnOrdered()
     {
         var existingProducts = ProductTestData.GenerateProducts(10);
         Context.Products.AddRange(existingProducts);
         await Context.SaveChangesAsync();
 
-        var supportedOrderingOptions = new (string, Func<IEnumerable<ProductEntity>, IEnumerable<ProductEntity>>)[]
-        {
-            ("TitleAscending", products => products.OrderBy(product => product.Title)),
-            ("TitleDescending", products => products.OrderByDescending(product => product.Title)),
-            ("PriceAscending", products => products.OrderBy(product => product.Price)),
-            ("PriceDescending", products => products.OrderByDescending(product => product.Price))
-        };
+        var requestUri =
+            "/products" +
+            "?OrderingOption=TitleAscending" +
+            "&PageNumber=0" +
+            $"&PageSize={existingProducts.Count}";
+        var response = await HttpClient.GetAsync(requestUri);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        foreach (var supportedOrderingOption in supportedOrderingOptions)
-        {
-            var (orderingOptionName, sortProducts) = supportedOrderingOption;
+        var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+        productsPageDto!.ProductsCount.Should().Be(existingProducts.Count);
+        productsPageDto.PagesCount.Should().Be(1);
 
-            var requestUri =
-                "/products" +
-                $"?OrderingOption={orderingOptionName}" +
-                "&PageNumber=0" +
-                $"&PageSize={existingProducts.Count}";
-            var response = await HttpClient.GetAsync(requestUri);
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var orderedExistingProducts = existingProducts.OrderBy(product => product.Title);
+        var productDtos = productsPageDto.Products;
+        productDtos.Should().BeEquivalentTo(orderedExistingProducts, options => options.WithStrictOrdering());
+    }
 
-            var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
-            productsPageDto!.ProductsCount.Should().Be(existingProducts.Count);
-            productsPageDto.PagesCount.Should().Be(1);
+    [Fact]
+    public async Task GetProducts_WhenTitleDescendingOrderingApplied_ShouldReturnOrdered()
+    {
+        var existingProducts = ProductTestData.GenerateProducts(10);
+        Context.Products.AddRange(existingProducts);
+        await Context.SaveChangesAsync();
 
-            var orderedExistingProducts = sortProducts(existingProducts);
-            var productDtos = productsPageDto.Products;
-            productDtos.Should().BeEquivalentTo(orderedExistingProducts, options => options.WithStrictOrdering());
-        }
+        var requestUri =
+            "/products" +
+            "?OrderingOption=TitleDescending" +
+            "&PageNumber=0" +
+            $"&PageSize={existingProducts.Count}";
+        var response = await HttpClient.GetAsync(requestUri);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+        productsPageDto!.ProductsCount.Should().Be(existingProducts.Count);
+        productsPageDto.PagesCount.Should().Be(1);
+
+        var orderedExistingProducts = existingProducts.OrderByDescending(product => product.Title);
+        var productDtos = productsPageDto.Products;
+        productDtos.Should().BeEquivalentTo(orderedExistingProducts, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenPriceAscendingOrderingApplied_ShouldReturnOrdered()
+    {
+        var existingProducts = ProductTestData.GenerateProducts(10);
+        Context.Products.AddRange(existingProducts);
+        await Context.SaveChangesAsync();
+
+        var requestUri =
+            "/products" +
+            "?OrderingOption=PriceAscending" +
+            "&PageNumber=0" +
+            $"&PageSize={existingProducts.Count}";
+        var response = await HttpClient.GetAsync(requestUri);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+        productsPageDto!.ProductsCount.Should().Be(existingProducts.Count);
+        productsPageDto.PagesCount.Should().Be(1);
+
+        var orderedExistingProducts = existingProducts.OrderBy(product => product.Price);
+        var productDtos = productsPageDto.Products;
+        productDtos.Should().BeEquivalentTo(orderedExistingProducts, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenPriceDescendingOrderingApplied_ShouldReturnOrdered()
+    {
+        var existingProducts = ProductTestData.GenerateProducts(10);
+        Context.Products.AddRange(existingProducts);
+        await Context.SaveChangesAsync();
+
+        var requestUri =
+            "/products" +
+            "?OrderingOption=PriceDescending" +
+            "&PageNumber=0" +
+            $"&PageSize={existingProducts.Count}";
+        var response = await HttpClient.GetAsync(requestUri);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+        productsPageDto!.ProductsCount.Should().Be(existingProducts.Count);
+        productsPageDto.PagesCount.Should().Be(1);
+
+        var orderedExistingProducts = existingProducts.OrderByDescending(product => product.Price);
+        var productDtos = productsPageDto.Products;
+        productDtos.Should().BeEquivalentTo(orderedExistingProducts, options => options.WithStrictOrdering());
     }
 
     [Fact]
@@ -57,7 +116,7 @@ public class GetProductsTestSuite : TestSuiteBase
                 priceRange: new() { Min = maxPrice + 0.01 }
             ),
             ThatWillNotBeFilteredOut = ProductTestData.GenerateProducts(
-                count: 15,
+                count: 10,
                 priceRange: new() { Min = minPrice, Max = maxPrice }
             )
         };
@@ -66,35 +125,93 @@ public class GetProductsTestSuite : TestSuiteBase
         Context.Products.AddRange(existingProducts.ThatWillNotBeFilteredOut);
         await Context.SaveChangesAsync();
 
-        var pageNumber = 1;
-        var pageSize = 10;
-
         var requestUri =
             "/products" +
             "?OrderingOption=TitleAscending" +
-            $"&PageNumber={pageNumber}" +
-            $"&PageSize={pageSize}" +
+            "&PageNumber=0" +
+            $"&PageSize={existingProducts.ThatWillNotBeFilteredOut.Count}" +
             $"&MinPrice={minPrice}" +
             $"&MaxPrice={maxPrice}";
         var response = await HttpClient.GetAsync(requestUri);
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
         var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
-
-        var expectedProductsCount = existingProducts.ThatWillNotBeFilteredOut.Count;
-        productsPageDto!.ProductsCount.Should().Be(expectedProductsCount);
-
-        var expectedPagesCount = (int)Math.Ceiling((double)expectedProductsCount / pageSize);
-        productsPageDto.PagesCount.Should().Be(expectedPagesCount);
+        productsPageDto!.ProductsCount.Should().Be(existingProducts.ThatWillNotBeFilteredOut.Count);
+        productsPageDto.PagesCount.Should().Be(1);
 
         var expectedProducts = existingProducts
             .ThatWillNotBeFilteredOut
             .OrderBy(product => product.Title)
-            .Skip(pageNumber * pageSize)
-            .Take(pageSize)
             .ToList();
         var returnedProductDtos = productsPageDto.Products;
         returnedProductDtos.Should().BeEquivalentTo(expectedProducts, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenRequestedLastPageNotFull_ShouldReturnNotFullPage()
+    {
+        var productsCount = 15;
+        var pageSize = 10;
+
+        var existingProducts = ProductTestData.GenerateProducts(productsCount);
+        Context.Products.AddRange(existingProducts);
+        await Context.SaveChangesAsync();
+
+        var expectedPagesCount = CalculateExpectedPagesCount(productsCount, pageSize);
+        var lastPageNumber = expectedPagesCount - 1;
+
+        var requestUri =
+            "/products" +
+            "?OrderingOption=TitleAscending" +
+            $"&PageNumber={lastPageNumber}" +
+            $"&PageSize={pageSize}";
+        var response = await HttpClient.GetAsync(requestUri);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+        productsPageDto!.PagesCount.Should().Be(expectedPagesCount);
+        productsPageDto.ProductsCount.Should().Be(productsCount);
+
+        var expectedProducts = existingProducts
+            .OrderBy(product => product.Title)
+            .Skip(pageSize)
+            .Take(pageSize);
+        var returnedProducts = productsPageDto.Products;
+        returnedProducts.Should().BeEquivalentTo(expectedProducts, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenMiddlePageIsRequested_ShouldReturnFullMiddlePage()
+    {
+        var productsCount = 25;
+        var pageSize = 10;
+        var middlePageNumber = 1;
+
+        var existingProducts = ProductTestData.GenerateProducts(productsCount);
+        Context.Products.AddRange(existingProducts);
+        await Context.SaveChangesAsync();
+
+        var requestUri =
+            "/products" +
+            "?OrderingOption=TitleAscending" +
+            $"&PageNumber={middlePageNumber}" +
+            $"&PageSize={pageSize}";
+        var response = await HttpClient.GetAsync(requestUri);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
+
+        var expectedPagesCount = CalculateExpectedPagesCount(productsCount, pageSize);
+        productsPageDto!.PagesCount.Should().Be(expectedPagesCount);
+
+        productsPageDto.ProductsCount.Should().Be(productsCount);
+
+        var expectedProducts = existingProducts
+            .OrderBy(product => product.Title)
+            .Skip(pageSize * middlePageNumber)
+            .Take(pageSize);
+        var returnedProducts = productsPageDto.Products;
+        returnedProducts.Should().BeEquivalentTo(expectedProducts, options => options.WithStrictOrdering());
     }
 
     [Fact]
@@ -120,10 +237,13 @@ public class GetProductsTestSuite : TestSuiteBase
         Context.Products.Add(existingProduct);
         await Context.SaveChangesAsync();
 
-        var minPrice = existingProduct.Price + 0.01;
-        var response = await HttpClient.GetAsync(
-            $"/products?OrderingOption=TitleAscending&PageNumber=0&PageSize=10&MinPrice={minPrice}"
-        );
+        var requestUri =
+            "/products" +
+            "?OrderingOption=TitleAscending" +
+            "&PageNumber=0" +
+            "&PageSize=10" +
+            $"&MinPrice={existingProduct.Price + 0.01}";
+        var response = await HttpClient.GetAsync(requestUri);
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
         var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
@@ -139,10 +259,13 @@ public class GetProductsTestSuite : TestSuiteBase
         Context.Products.Add(existingProduct);
         await Context.SaveChangesAsync();
 
-        var maxPrice = existingProduct.Price - 0.01;
-        var response = await HttpClient.GetAsync(
-            $"/products?OrderingOption=TitleAscending&PageNumber=0&PageSize=10&MaxPrice={maxPrice}"
-        );
+        var requestUri =
+            "/products" +
+            "?OrderingOption=TitleAscending" +
+            "&PageNumber=0" +
+            "&PageSize=10" +
+            $"&MaxPrice={existingProduct.Price - 0.01}";
+        var response = await HttpClient.GetAsync(requestUri);
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
         var productsPageDto = await response.Content.ReadFromJsonAsync<ProductsPageDto>();
@@ -162,4 +285,7 @@ public class GetProductsTestSuite : TestSuiteBase
         productsPageDto.ProductsCount.Should().Be(0);
         productsPageDto.Products.Should().BeEmpty();
     }
+
+    private static int CalculateExpectedPagesCount(int productsCount, int pageSize) =>
+        (int)Math.Ceiling((double)productsCount / pageSize);
 }
