33e50af99ddb1f3b85a988f04cf727ccda1d48ec
Bogdan Shahnitsky <satma0745@gmail.com>
2024-05-12 21:42:55 +0300

    Specify the only supported media type: `application/json`.

--- Tsk.Auth.HttpApi/Features/ApiControllerBase.cs
+++ Tsk.Auth.HttpApi/Features/ApiControllerBase.cs
@@ -1,6 +1,9 @@
+using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Tsk.Auth.HttpApi.Features;
 
 [ApiController]
+[Consumes(MediaTypeNames.Application.Json)]
+[Produces(MediaTypeNames.Application.Json)]
 public abstract class ApiControllerBase : ControllerBase;
