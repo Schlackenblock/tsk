79806a7a7c9a6dbac0b58e1d5a92f789ee8aa52b
Bogdan Shahnitsky <satma0745@gmail.com>
2024-05-14 23:20:44 +0300

    Implement the `SignOutEverywhere` feature.

---
+++ Tsk.Auth.HttpApi/Features/SignOutEverywhere.cs
@@ -0,0 +1,36 @@
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tsk.Auth.HttpApi.Context;
+
+namespace Tsk.Auth.HttpApi.Features;
+
+public static class SignOutEverywhereFeature
+{
+    public sealed class Controller : ApiControllerBase
+    {
+        private readonly TskAuthContext dbContext;
+
+        public Controller(TskAuthContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost("/sign-out-everywhere")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> SignOutEverywhere()
+        {
+            var userIdClaim = User.Claims.Single(claim => claim.Type == JwtRegisteredClaimNames.Sub);
+            var userId = Guid.Parse(userIdClaim.Value);
+
+            await dbContext.Sessions
+                .Where(session => session.UserId == userId)
+                .ExecuteDeleteAsync();
+
+            return Ok();
+        }
+    }
+}
