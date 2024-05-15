4352f9e18b086525d8bc211cf93f2aec03532445
Bogdan Shahnitsky <satma0745@gmail.com>
2024-05-15 18:41:22 +0300

    Refactor the `WhoAmI` feature.

--- Tsk.Auth.HttpApi/Features/WhoAmI.cs
+++ Tsk.Auth.HttpApi/Features/WhoAmI.cs
@@ -1,33 +1,52 @@
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Tsk.Auth.HttpApi.AspInfrastructure;
+using Tsk.Auth.HttpApi.Context;
 
 namespace Tsk.Auth.HttpApi.Features;
 
 public static class WhoAmIFeature
 {
-    public sealed class AccessTokenPayloadDto
+    public sealed class CurrentUserDto
     {
         public required Guid Id { get; init; }
+        public required string Email { get; init; }
     }
 
     public sealed class Controller : ApiControllerBase
     {
+        private readonly TskAuthContext dbContext;
+
+        public Controller(TskAuthContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         [HttpGet("/who-am-i")]
         [Authorize]
-        [ProducesResponseType<AccessTokenPayloadDto>(StatusCodes.Status200OK)]
+        [ProducesResponseType<CurrentUserDto>(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public IActionResult WhoAmI()
+        public async Task<IActionResult> WhoAmI()
         {
             var userIdClaim = User.Claims.Single(claim => claim.Type == JwtRegisteredClaimNames.Sub);
             var userId = Guid.Parse(userIdClaim.Value);
 
-            var accessTokenPayloadDto = new AccessTokenPayloadDto
+            var user = await dbContext.Users
+                .AsNoTracking()
+                .SingleOrDefaultAsync(user => user.Id == userId);
+            if (user is null)
             {
-                Id = userId
+                return Unauthorized();
+            }
+
+            var currentUserDto = new CurrentUserDto
+            {
+                Id = user.Id,
+                Email = user.Email
             };
-            return Ok(accessTokenPayloadDto);
+            return Ok(currentUserDto);
         }
     }
 }
