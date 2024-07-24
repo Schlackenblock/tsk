89b03ec3a51e90350996b5916c540cc29de3fe99
Bogdan Shahnitsky <satma0745@gmail.com>
2024-07-24 21:08:50 +0300

    Create the migration.

---
+++ Tsk.HttpApi/Migrations/20240724180553_AddIsForSale.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tsk.HttpApi.Migrations
+{
+    public partial class AddIsForSale : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_for_sale",
+                table: "products",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_for_sale",
+                table: "products"
+            );
+        }
+    }
+}
