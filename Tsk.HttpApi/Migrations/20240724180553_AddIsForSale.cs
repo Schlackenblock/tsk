a4b9ac2e7fe3ca1a3294a2426c07642dc6c6418c
Bogdan Shahnitsky <satma0745@gmail.com>
2024-07-24 21:11:21 +0300

    Fix the migration.

--- Tsk.HttpApi/Migrations/20240724180553_AddIsForSale.cs
+++ Tsk.HttpApi/Migrations/20240724180553_AddIsForSale.cs
@@ -12,8 +12,21 @@ namespace Tsk.HttpApi.Migrations
                 name: "is_for_sale",
                 table: "products",
                 type: "boolean",
-                nullable: false,
-                defaultValue: false
+                nullable: true
+            );
+
+            migrationBuilder.Sql(
+                """
+                UPDATE products
+                SET is_for_sale = FALSE;
+                """
+            );
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_for_sale",
+                table: "products",
+                oldNullable: true,
+                nullable: false
             );
         }
 
