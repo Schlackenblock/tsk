85750840878c3dea226273987548d9354e360b3d
Bogdan Shahnitsky <satma0745@gmail.com>
2024-08-03 16:56:13 +0300

    Test pagination.

--- Tsk.Tests/Products/ForCustomers/GetProductsTestSuite.cs
+++ Tsk.Tests/Products/ForCustomers/GetProductsTestSuite.cs
@@ -6,6 +6,188 @@ namespace Tsk.Tests.Products.ForCustomers;
 public class GetProductsTestSuite : IntegrationTestSuiteBase
 {
     [Fact]
+    public async Task GetProducts_WhenFirstPageRequested_ShouldReturnOnlyFirstPage()
+    {
+        var products = new[]
+        {
+            new Product { Id = Guid.NewGuid(), Code = "P1", Title = "Product #1", Price = 1.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P2", Title = "Product #2", Price = 2.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P3", Title = "Product #3", Price = 3.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P4", Title = "Product #4", Price = 4.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P5", Title = "Product #5", Price = 5.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P6", Title = "Product #6", Price = 6.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P7", Title = "Product #7", Price = 7.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P8", Title = "Product #8", Price = 8.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P9", Title = "Product #9", Price = 9.99m, IsForSale = true }
+        };
+
+        await CallDbAsync(async dbContext =>
+        {
+            dbContext.Products.AddRange(products);
+            await dbContext.SaveChangesAsync();
+        });
+
+        var response = await HttpClient.GetAsync("/products?orderBy=price_asc&pageNumber=0&pageSize=3");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var expectedProductDtos = products
+            .OrderBy(product => product.Price)
+            .Take(3)
+            .Select(product => new ProductDto
+            {
+                Id = product.Id,
+                Code = product.Code,
+                Title = product.Title,
+                Price = product.Price
+            });
+
+        var productDtos = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+        productDtos.Should().BeEquivalentTo(expectedProductDtos, config => config.WithStrictOrdering());
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenMiddlePageRequested_ShouldReturnOnlyMiddlePage()
+    {
+        var products = new[]
+        {
+            new Product { Id = Guid.NewGuid(), Code = "P1", Title = "Product #1", Price = 1.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P2", Title = "Product #2", Price = 2.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P3", Title = "Product #3", Price = 3.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P4", Title = "Product #4", Price = 4.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P5", Title = "Product #5", Price = 5.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P6", Title = "Product #6", Price = 6.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P7", Title = "Product #7", Price = 7.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P8", Title = "Product #8", Price = 8.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P9", Title = "Product #9", Price = 9.99m, IsForSale = true }
+        };
+
+        await CallDbAsync(async dbContext =>
+        {
+            dbContext.Products.AddRange(products);
+            await dbContext.SaveChangesAsync();
+        });
+
+        var response = await HttpClient.GetAsync("/products?orderBy=price_asc&pageNumber=1&pageSize=3");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var expectedProductDtos = products
+            .OrderBy(product => product.Price)
+            .Skip(3)
+            .Take(3)
+            .Select(product => new ProductDto
+            {
+                Id = product.Id,
+                Code = product.Code,
+                Title = product.Title,
+                Price = product.Price
+            });
+
+        var productDtos = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+        productDtos.Should().BeEquivalentTo(expectedProductDtos, config => config.WithStrictOrdering());
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenLastPageRequested_ShouldReturnOnlyLastPage()
+    {
+        var products = new[]
+        {
+            new Product { Id = Guid.NewGuid(), Code = "P1", Title = "Product #1", Price = 1.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P2", Title = "Product #2", Price = 2.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P3", Title = "Product #3", Price = 3.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P4", Title = "Product #4", Price = 4.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P5", Title = "Product #5", Price = 5.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P6", Title = "Product #6", Price = 6.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P7", Title = "Product #7", Price = 7.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P8", Title = "Product #8", Price = 8.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P9", Title = "Product #9", Price = 9.99m, IsForSale = true }
+        };
+
+        await CallDbAsync(async dbContext =>
+        {
+            dbContext.Products.AddRange(products);
+            await dbContext.SaveChangesAsync();
+        });
+
+        var response = await HttpClient.GetAsync("/products?orderBy=price_asc&pageNumber=2&pageSize=3");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var expectedProductDtos = products
+            .OrderBy(product => product.Price)
+            .Skip(6)
+            .Take(3)
+            .Select(product => new ProductDto
+            {
+                Id = product.Id,
+                Code = product.Code,
+                Title = product.Title,
+                Price = product.Price
+            });
+
+        var productDtos = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+        productDtos.Should().BeEquivalentTo(expectedProductDtos, config => config.WithStrictOrdering());
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenNotExistingPageRequested_ShouldReturnEmptyPage()
+    {
+        var products = new[]
+        {
+            new Product { Id = Guid.NewGuid(), Code = "P1", Title = "Product #1", Price = 1.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P2", Title = "Product #2", Price = 2.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P3", Title = "Product #3", Price = 3.99m, IsForSale = true }
+        };
+
+        await CallDbAsync(async dbContext =>
+        {
+            dbContext.Products.AddRange(products);
+            await dbContext.SaveChangesAsync();
+        });
+
+        var response = await HttpClient.GetAsync("/products?orderBy=price_asc&pageNumber=1&pageSize=3");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var productDtos = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+        productDtos.Should().BeEquivalentTo(Enumerable.Empty<ProductDto>());
+    }
+
+    [Fact]
+    public async Task GetProducts_WhenPartiallyFilledPageRequested_ShouldReturnPartiallyFilledPage()
+    {
+        var products = new[]
+        {
+            new Product { Id = Guid.NewGuid(), Code = "P1", Title = "Product #1", Price = 1.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P2", Title = "Product #2", Price = 2.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P3", Title = "Product #3", Price = 3.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P4", Title = "Product #4", Price = 4.99m, IsForSale = true },
+            new Product { Id = Guid.NewGuid(), Code = "P5", Title = "Product #5", Price = 5.99m, IsForSale = true }
+        };
+
+        await CallDbAsync(async dbContext =>
+        {
+            dbContext.Products.AddRange(products);
+            await dbContext.SaveChangesAsync();
+        });
+
+        var response = await HttpClient.GetAsync("/products?orderBy=price_asc&pageNumber=1&pageSize=3");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var expectedProductDtos = products
+            .OrderBy(product => product.Price)
+            .Skip(3)
+            .Take(2)
+            .Select(product => new ProductDto
+            {
+                Id = product.Id,
+                Code = product.Code,
+                Title = product.Title,
+                Price = product.Price
+            });
+
+        var productDtos = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+        productDtos.Should().BeEquivalentTo(expectedProductDtos, config => config.WithStrictOrdering());
+    }
+
+    [Fact]
     public async Task GetProducts_WhenOrderedByPriceAscending_ShouldReturnOrdered()
     {
         var products = new[]
