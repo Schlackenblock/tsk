412ae9002955742d811614ea15f2a97440924da7
Bogdan Shahnitsky <satma0745@gmail.com>
2024-08-04 16:25:43 +0300

    Create the migration.

---
+++ Tsk.HttpApi/Migrations/20240804131440_AddProductPictures.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tsk.HttpApi.Migrations
+{
+    public partial class AddProductPictures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<string>>(
+                name: "pictures",
+                table: "products",
+                type: "text[]",
+                nullable: true
+            );
+
+            migrationBuilder.Sql(
+                """
+                UPDATE products
+                SET pictures = '{}';
+                """
+            );
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "pictures",
+                table: "products",
+                oldNullable: true,
+                nullable: false
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "pictures",
+                table: "products"
+            );
+        }
+    }
+}
