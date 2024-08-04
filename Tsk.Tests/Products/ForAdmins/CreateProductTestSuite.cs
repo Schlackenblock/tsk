8ba2da8ad89423f1fa4bcb0c91003eeae696cb1c
Bogdan Shahnitsky <satma0745@gmail.com>
2024-08-04 16:33:26 +0300

    Test product pictures behavior on product creation.

--- Tsk.Tests/Products/ForAdmins/CreateProductTestSuite.cs
+++ Tsk.Tests/Products/ForAdmins/CreateProductTestSuite.cs
@@ -6,38 +6,112 @@ namespace Tsk.Tests.Products.ForAdmins;
 public class CreateProductTestSuite : IntegrationTestSuiteBase
 {
     [Fact]
-    public async Task CreateProduct_WhenValid_ShouldSucceed()
+    public async Task CreateProduct_WithMultiplePictures_ShouldSucceed()
     {
-        var createProductDto = new CreateProductDto { Code = "P", Title = "Product", Pictures = ["Product Picture"], Price = 9.99m };
+        var createProductDto = new CreateProductDto { Code = "P", Title = "Product", Pictures = ["Picture #1", "Picture #2"], Price = 9.99m };
 
         var response = await HttpClient.PostAsJsonAsync("/management/products", createProductDto);
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
         var createdProductDto = await response.Content.ReadFromJsonAsync<ProductDto>();
         createdProductDto.Should().BeEquivalentTo(
-            new Product
+            new ProductDto
             {
                 Id = default,
                 Code = createProductDto.Code,
                 Title = createProductDto.Title,
                 Pictures = createProductDto.Pictures,
-                Price = createProductDto.Price,
-                IsForSale = false
+                IsForSale = false,
+                Price = createProductDto.Price
             },
             config => config.Excluding(product => product.Id)
         );
 
         await CallDbAsync(async dbContext =>
         {
-            var persistedProduct = await dbContext.Products.SingleAsync();
-            persistedProduct.Should().BeEquivalentTo(new Product
+            var createdProduct = await dbContext.Products.SingleAsync();
+            createdProduct.Should().BeEquivalentTo(new Product
             {
                 Id = createdProductDto!.Id,
                 Code = createdProductDto.Code,
                 Title = createdProductDto.Title,
                 Pictures = createdProductDto.Pictures,
-                Price = createdProductDto.Price,
-                IsForSale = createdProductDto.IsForSale
+                IsForSale = false,
+                Price = createdProductDto.Price
+            });
+        });
+    }
+
+    [Fact]
+    public async Task CreateProduct_WithSinglePicture_ShouldSucceed()
+    {
+        var createProductDto = new CreateProductDto { Code = "P", Title = "Product", Pictures = ["Picture"], Price = 9.99m };
+
+        var response = await HttpClient.PostAsJsonAsync("/management/products", createProductDto);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var createdProductDto = await response.Content.ReadFromJsonAsync<ProductDto>();
+        createdProductDto.Should().BeEquivalentTo(
+            new ProductDto
+            {
+                Id = default,
+                Code = createProductDto.Code,
+                Title = createProductDto.Title,
+                Pictures = createProductDto.Pictures,
+                IsForSale = false,
+                Price = createProductDto.Price
+            },
+            config => config.Excluding(product => product.Id)
+        );
+
+        await CallDbAsync(async dbContext =>
+        {
+            var createdProduct = await dbContext.Products.SingleAsync();
+            createdProduct.Should().BeEquivalentTo(new Product
+            {
+                Id = createdProductDto!.Id,
+                Code = createdProductDto.Code,
+                Title = createdProductDto.Title,
+                Pictures = createdProductDto.Pictures,
+                IsForSale = false,
+                Price = createdProductDto.Price
+            });
+        });
+    }
+
+    [Fact]
+    public async Task CreateProduct_WithoutPictures_ShouldSucceed()
+    {
+        var createProductDto = new CreateProductDto { Code = "P", Title = "Product", Pictures = [], Price = 9.99m };
+
+        var response = await HttpClient.PostAsJsonAsync("/management/products", createProductDto);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var createdProductDto = await response.Content.ReadFromJsonAsync<ProductDto>();
+        createdProductDto.Should().BeEquivalentTo(
+            new ProductDto
+            {
+                Id = default,
+                Code = createProductDto.Code,
+                Title = createProductDto.Title,
+                Pictures = createProductDto.Pictures,
+                IsForSale = false,
+                Price = createProductDto.Price
+            },
+            config => config.Excluding(product => product.Id)
+        );
+
+        await CallDbAsync(async dbContext =>
+        {
+            var createdProduct = await dbContext.Products.SingleAsync();
+            createdProduct.Should().BeEquivalentTo(new Product
+            {
+                Id = createdProductDto!.Id,
+                Code = createdProductDto.Code,
+                Title = createdProductDto.Title,
+                Pictures = createdProductDto.Pictures,
+                IsForSale = false,
+                Price = createdProductDto.Price
             });
         });
     }
