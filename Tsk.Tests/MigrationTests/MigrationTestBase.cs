a1f2549ab73f5cf1e79408058c449aa685e0cc96
Bogdan Shahnitsky <satma0745@gmail.com>
2024-08-05 18:28:24 +0300

    Add simple all-migration tests.

---
+++ Tsk.Tests/MigrationTests/MigrationTestBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Testcontainers.PostgreSql;
+using Tsk.HttpApi;
+
+namespace Tsk.Tests.MigrationTests;
+
+public abstract class MigrationTestBase : IAsyncLifetime
+{
+    protected IMigrator Migrator { get; private set; } = null!;
+    protected List<string> Migrations { get; private set; } = null!;
+
+    private readonly PostgreSqlContainer postgreSqlContainer =
+        new PostgreSqlBuilder()
+            .WithImage("postgres:latest")
+            .Build();
+
+    private TskDbContext dbContext = null!;
+
+    public async Task InitializeAsync()
+    {
+        await postgreSqlContainer.StartAsync();
+
+        var dbContextOptions = new DbContextOptionsBuilder<TskDbContext>()
+            .UseNpgsql(postgreSqlContainer.GetConnectionString())
+            .Options;
+
+        dbContext = new TskDbContext(dbContextOptions);
+
+        Migrator = dbContext.GetService<IMigrator>();
+        Migrations = dbContext.Database.GetMigrations().ToList();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await dbContext.DisposeAsync();
+        await postgreSqlContainer.StopAsync();
+    }
+}
