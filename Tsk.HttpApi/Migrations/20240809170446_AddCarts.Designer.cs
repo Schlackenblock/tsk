866c183a07fc07d9ea3be2e3e3752699b27874ed
Bogdan Shahnitsky <satma0745@gmail.com>
2024-08-09 20:15:35 +0300

    Add migration.

--- Tsk.HttpApi/Migrations/20240809170446_AddCarts.Designer.cs
+++ Tsk.HttpApi/Migrations/20240809170446_AddCarts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Tsk.HttpApi;
@@ -12,9 +13,11 @@ using Tsk.HttpApi;
 namespace Tsk.HttpApi.Migrations
 {
     [DbContext(typeof(TskDbContext))]
-    partial class TskDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240809170446_AddCarts")]
+    partial class AddCarts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
